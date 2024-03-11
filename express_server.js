d1a6a44016acb40dc7b5ae644c868d13996a62a0
ItsAhmad <ahmad.ameen526@hotmail.com>
2024-03-10 21:02:47 -0400

    added endpoint to handle a POST to /login in express server

--- express_server.js
+++ express_server.js
@@ -67,6 +67,14 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+
+  res.cookie("username", username);
+  
+  res.redirect("/urls");
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
